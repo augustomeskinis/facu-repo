1d8e10dbef068869be16e47ef379468058919841
augusto <augustomeskinis05@gmail.com>
2025-03-23 11:00:05 -0300

    cambie el tipo de proyecto de ayed e hice unos ejercicios de FOD

---
+++ AyED/ejercicios de practica resueltos/src/ejercicio3/Profesor.java
@@ -0,0 +1,68 @@
+package tp1.ejercicio3;
+
+public class Profesor {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private int catedra;
+    private String facultad;
+
+    public Profesor (String nombre, String apellido, String email,
+                    int catedra, String facultad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.catedra = catedra;
+        this.facultad = facultad;
+    }
+
+
+
+    public String tusDatos (){
+        return this.nombre +
+                this.getApellido() + "/n"+
+                this.getNombre() + "/n" +
+                this.getCatedra() + "/n" +
+                this.getEmail() + "/n" +
+                this.getFacultad() + "/n" ;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public int getCatedra() {
+        return catedra;
+    }
+
+    public void setCatedra(int catedra) {
+        this.catedra = catedra;
+    }
+}
---
+++ AyED/ejercicios de practica resueltos/src/ejercicio3/Test.java
@@ -0,0 +1,31 @@
+package tp1.ejercicio3;
+
+public class Test {
+    public static void main (String [] args){
+        Estudiante estudiantes [] = new Estudiante[2];
+        Profesor profesores [] = new Profesor[3];
+
+
+        estudiantes[0] = new Estudiante("pepe","gomez"
+        , 1, "pepe@yahoo.com", "calle 2 n4");
+        estudiantes[1] = new Estudiante("juan","gomez"
+                , 2, "juan@yahoo.com", "calle 5 n3");
+        profesores[0] = new Profesor("walter","herman",
+                "walterherman@yahoo.com", 3, "informatica");
+        profesores[1] = new Profesor("gonza","villarueal",
+                "gonzavillaruel@yahoo.com", 2, "informatica");
+        profesores[2] = new Profesor("marta","lopez",
+                "martalopez@yahoo.com", 9, "informatica");
+
+        for (int i=0;i<2;i++){
+            System.out.println(estudiantes[i].tusDatos());
+        }
+        for (int i=0;i<3;i++){
+            System.out.println(profesores[i].tusDatos());
+        }
+    }
+
+
+
+
+}
